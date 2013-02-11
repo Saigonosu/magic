e126691267f9d69caa8c653fed6af7d6fa889dc6
Live session user <mint@mint.(none)>
2013-02-11 04:32:15 +0000

    Added magic

---
+++ magic.rb
@@ -0,0 +1,22 @@
+cardlist = Hash.new
+
+price_list = File.new("/home/mint/current-magic", "r")
+
+while (line = price_list.gets)
+	cardlist[line.split(",")[0]] = line.split(",")[1].to_f
+end
+
+
+price = 0.0
+
+puts "Enter the name of the list you would like to tally"
+x = gets.chomp
+list = File.new(x, "r")
+
+while(card = list.gets)
+	puts card.chomp
+	puts cardlist[card.chomp]
+	price = price + cardlist[card.chomp]
+end
+
+puts "The value of the collection is $#{price.round(2)}"
