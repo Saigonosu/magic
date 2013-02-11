d181e67aeaf5e4f1b5bc65bff6f33de65c936bc4
Brad Chase <bosco87@gmail.com>
2013-02-11 00:11:42 -0500

    create Card class, use File.each to loop input list

--- magic.rb
+++ magic.rb
@@ -1,22 +1,41 @@
+# class Card
+#   attr_accessor :name, :price
+#   def initialize(name, price)
+#     @name, @price = name, price
+#   end
+# end
+#
+# is equivalent to...
+Card = Struct.new(:name, :price)
+
+
 cardlist = Hash.new
 
 price_list = File.new("./cards.csv", "r")
 
 while (line = price_list.gets)
-	cardlist[line.split(",")[0]] = line.split(",")[1].to_f
+  name, price = line.split(",")
+  price = price.to_f
+
+  card = Card.new name, price
+
+	cardlist[card.name] = card
 end
 
-
-price = 0.0
+total_price = 0.0
 
 puts "Enter the name of the list you would like to tally"
-x = gets.chomp
-list = File.new(x, "r")
+file_path = gets.chomp
+File.new(file_path, "r").each do |line|
+  card_name = line.chomp
 
-while(card = list.gets)
-	puts card.chomp
-	puts cardlist[card.chomp]
-	price = price + cardlist[card.chomp]
+	puts card_name
+	puts cardlist[card_name]
+
+  card  = cardlist[card_name]
+  price = card.price
+
+	total_price += price
 end
 
-puts "The value of the collection is $#{price.round(2)}"
+puts "The value of the collection is $#{total_price.round(2)}"
