98d8c70d68679e185a6ee0edc5276ce33ec1c177
Live session user <mint@mint.(none)>
2013-02-12 05:29:11 +0000

    Added a csv file for all magic cards with all attributes. Tried to implement a script similar to price except returning the full attributes of the cards. Hitting a snag with the hash. Created a seperate ruby file so as to not wreck our current file

---
+++ magictest.rb
@@ -0,0 +1,74 @@
+module Magic
+  @@cards = {}
+  @@fullcard = {}
+
+  def self.cards
+    @@cards
+  end
+
+  def self.fullcard
+	@@fullcard
+  end
+
+  def @@cards.<<(card)
+    @@cards[card.name] = card
+  end
+
+  def @@fullcard.<<(card)
+    @@fullcard[card.name] = card
+  end
+
+  # class Card
+  #   attr_accessor :name, :price
+  #   def initialize(name, price)
+  #     @name, @price = name, price
+  #   end
+  # end
+  #
+  # is equivalent to...
+  Card = Struct.new(:name, :price) do
+    def self.create_from_line_of_csv(csv_line)
+      name, price = csv_line.split(",")
+      price = price.to_f
+
+      card = self.new name, price
+
+      Magic.cards << card
+    end
+  end
+
+	FullCard = Struct.new(:name, :set, :rarity, :cost, :pt, :type, :text, :flavor) do
+	def self.create_from_line_of_csv(full_list)
+     name, set, rarity, cost, pt, type, text, flavor = full_list.split(",")
+     card = self.new name, set, rarity, cost, pt, type, text, flavor
+     Magic.fullcard << card
+    end
+end
+end
+# generate all Magic::Card
+File.new("./cards.csv", "r").each do |line|
+  Magic::Card.create_from_line_of_csv(line)
+end
+puts Magic.cards
+# generate all full Magic::FullCard
+File.new("./Master.csv", "r").each do |line|
+  Magic::FullCard.create_from_line_of_csv(line)
+end
+puts Magic.fullcard
+total_price = 0.0
+
+puts "Enter the name of the list you would like to tally"
+file_path = gets.chomp
+File.new(file_path, "r").each do |line|
+  card_name = line.chomp
+
+  card  = Magic.cards[card_name]
+  fullcard = Magic.fullcard[card_name]
+  price = card.price
+
+	puts card.name, card.price, fullcard.set, fullcard.rarity, fullcard.cost, fullcard.pt, fullcard.type, fullcard.text
+
+	total_price += price
+end
+
+puts "The value of the collection is $#{total_price.round(2)}"
